a48edc9b21c1177e6cd29ce15a788d46adda849b
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-10-23 17:58:15 -0300

    Commit de client

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { PlayerPageComponent } from './views/plater-page/player-page.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
+import { SettingsPageComponent } from './views/settings-page/settings-page.component';
+import { authGuard } from './core/guards/auth.guard';
+
+export const routes: Routes = [
+  { path: 'players', component: PlayerPageComponent },
+  { path: 'account', component: LoginPageComponent },
+  {
+    path: 'settings',
+    component: SettingsPageComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'players',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'players',
+    pathMatch: 'full',
+  },
+];
---
+++ client/src/app/core/components/header/header.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { SearchService } from '../../services/search.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  isSearchVisible = false;
+  constructor(private router: Router, private searchService: SearchService) {}
+
+  onSearchInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.searchService.changeSearchTerm(input.value);
+  }
+
+  ngOnInit() {
+    this.updateSearchVisibility(this.router.url);
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateSearchVisibility(this.router.url);
+      });
+  }
+
+  updateSearchVisibility(url: string) {
+    this.isSearchVisible = url.includes('/players');
+  }
+}
